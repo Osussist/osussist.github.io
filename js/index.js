46e6c4ac5e5b4cb8ef01a2c423bea276f71e93aa
Takkeshi <prosaso12133@gmail.com>
2024-08-25 21:28:05 -0600

    chore: Update panel styles and layout

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function spawnHomePage()  {
+
+}
+
+function spawnCreditsPage()  {
+
+}
+
+function spawnSupportPage()  {
+
+}
+
+function spawnFeaturesPage() {
+
+}
+
+function spawnDownloadPage() {
+
+}
+
+function spawnChangelogPage() {
+
+}
+
+function spawnConfigPage() {
+
+}
+
+function spawnWikiPage() {
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    spawnHomePage();
+});
