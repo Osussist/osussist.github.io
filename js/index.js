1750a36423327162923a60ef89f43efaf3067347
LUXTACO <tacomastabusiness@gmail.com>
2024-11-10 21:02:36 -0600

    Refactor video hover effect to apply transformations directly to the video element; enhance shadow effects

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ function checkMobile() {
 
 function addVideoHoverEffect() {
     const videoContainer = document.querySelector('.video-rounding');
+    const video = videoContainer.querySelector('video');
+    videoContainer.style.overflow = 'visible';
     videoContainer.addEventListener('mousemove', (e) => {
-        videoContainer.style.transition = 'all 0.1s ease';
+        video.style.transition = 'all 0.1s ease';
         const rect = videoContainer.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
@@ -15,13 +17,13 @@ function addVideoHoverEffect() {
         const centerY = rect.height / 2;
         const rotateX = (centerY - y) / centerY * 10;
         const rotateY = (x - centerX) / centerX * 10;
-        videoContainer.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-        videoContainer.style.boxShadow = `${rotateY * 2}px ${rotateX * 2}px 20px rgba(0, 0, 0, 0.5)`;
+        video.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        video.style.boxShadow = `${rotateY * 2}px ${rotateX * 2}px 50px rgba(0, 0, 0, 0.5)`;
     });
     videoContainer.addEventListener('mouseleave', () => {
-        videoContainer.style.transition = 'all 0.5s ease';
-        videoContainer.style.transform = 'rotateY(0deg) rotateX(0deg)';
-        videoContainer.style.boxShadow = `0px 0px 20px rgba(0, 0, 0, 0)`;
+        video.style.transition = 'all 0.5s ease';
+        video.style.transform = 'rotateY(0deg) rotateX(0deg)';
+        video.style.boxShadow = `0px 0px 50px rgba(0, 0, 0, 0)`;
     });
 }
 
